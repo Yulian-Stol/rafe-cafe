af546989d2b9aa5a368d23b528ae851f302f5d9a
Yulian <kokomoko12345koko@gmail.com>
2022-11-30 16:04:10 +0200

    task

--- app/js/app.js
+++ app/js/app.js
@@ -21,6 +21,24 @@ document.addEventListener("DOMContentLoaded", function() {
 		};
 		headerFixed('.header', '.header--active');
 
+	//---menu---
+		const menu = (menuList, menuItem, menuBtn, menuApp) => {
+			const list = document.querySelector(menuList)
+			const item = document.querySelectorAll(menuItem)
+			const btn = document.querySelector(menuBtn)
+			const app = document.querySelector(menuApp)
+
+			item.forEach((e) => {
+				e.addEventListener('click', () => {
+					list.classList.remove('header--menu');
+					btn.classList.remove('hamburger--active');
+					app.classList.remove('header__nav--active');
+			})
+			});
+
+		}
+		menu('.header', '.tabs__item', '.header__btn-menu', '.header__item')
+
 	//----------------------HAMBURGER-----------------------
 		const hamburger = (hamburgerButton, hamburgerNav, hamburgerHeader) => {
 			const button = document.querySelector(hamburgerButton),
@@ -115,45 +133,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	};
 	modalslike('.modal-like');
-
-	//----------------------TABS-JS----------------------
-	const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
-		const header = document.querySelector(headerSelector),
-					tab = document.querySelectorAll(tabSelector),
-					content = document.querySelectorAll(contentSelector);
-
-		function hideTabContent() {
-			content.forEach(item => {
-				item.style.display = "none";
-			});
-
-			tab.forEach(item => {
-				item.classList.remove(activeClass);
-			});
-		}
-
-		function showTabContent(i = 0) {
-			content[i].style.display = "block";
-			tab[i].classList.add(activeClass);
-		}
-
-		hideTabContent();
-		showTabContent();
-
-		header.addEventListener('click', (e) => {
-			const target = e.target;
-			if (target && 
-				(target.classList.contains(tabSelector.replace(/\./, '')) || 
-				target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
-				tab.forEach((item, i) => {
-					if (target == item || target.parentNode == item) {
-						hideTabContent();
-						showTabContent(i);
-					}
-				});
-			}
-		});
-	};
-	tabs('.tabs', '.tabs__item', '.tabs__wrap', 'tabs--active');
 
 });
